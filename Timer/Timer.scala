566523f8de88db959b658b19f8712affe373bdd6
Alan Jennings <A.Jennings@kainos.com>
2016-08-14 16:20:23 +0100

    now using an anonymous inner class for timeflies method

--- Timer/Timer.scala
+++ Timer/Timer.scala
@@ -2,11 +2,9 @@ object Timer {
 	def oncePerSecond(callback: () => Unit) {
 		while (true) { callback(); Thread sleep 1000 }
 	}
-	def timeFlies() {
-		println("time flies like an arrow...")
-		println("Alan having a play...")
-	}
+	
 	def main(args: Array[String]) {
-		oncePerSecond(timeFlies)
+		oncePerSecond(() =>
+			println("time flies like an arrow..."))
 	}
 }
